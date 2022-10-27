43dcd8040742784cc7cef8b3430b76a2b9a89425
mathi <mathias_a_sandnes@hotmail.com>
2022-10-27 13:08:08 +0200

    merged

---
+++ src/augmentation.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+
+def augment(points, label):
+    # jitter points
+    points += tf.random.uniform(points.shape, -0.005, 0.005, dtype=tf.float64)
+    # shuffle points
+    points = tf.random.shuffle(points)
+    return points, label
--- src/main.py
+++ src/main.py
@@ -3,70 +3,32 @@ import glob
 import trimesh
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers
 from matplotlib import pyplot as plt
+
 from config import config
 from src.network.point_net import get_point_net_model
+from src.augmentation import augment
 
-"""
-Title: Point cloud classification with PointNet
-Author: [David Griffiths](https://dgriffiths3.github.io)
-Date created: 2020/05/25
-Last modified: 2020/05/26
-Description: Implementation of PointNet for ModelNet10 classification.
-"""
-"""
-# Point cloud classification
-"""
+tf.random.set_seed(config['random_seed'])
 
-"""
-## Introduction
-Classification, detection and segmentation of unordered 3D point sets i.e. point clouds
-is a core problem in computer vision. This example implements the seminal point cloud
-deep learning paper [PointNet (Qi et al., 2017)](https://arxiv.org/abs/1612.00593). For a
-detailed intoduction on PointNet see [this blog
-post](https://medium.com/@luis_gonzales/an-in-depth-look-at-pointnet-111d7efdaa1a).
-"""
-
-"""
-## Setup
-If using colab first install trimesh with `!pip install trimesh`.
-"""
-
-tf.random.set_seed(1234)
-
-DATA_DIR = 'C:/Users/guttu/Desktop/Dev/PyCharm/3D-deep-learning/data/ModelNet10'
-
-"""
-We can use the `trimesh` package to read and visualize the `.off` mesh files.
-"""
-
-mesh = trimesh.load(os.path.join(DATA_DIR, "chair/train/chair_0001.off"))
-mesh.show()
-
-"""
-To convert a mesh file to a point cloud we first need to sample points on the mesh
-surface. `.sample()` performs a unifrom random sampling. Here we sample at 2048 locations
-and visualize in `matplotlib`.
-"""
-
-points = mesh.sample(2048)
-
-fig = plt.figure(figsize=(5, 5))
-ax = fig.add_subplot(111, projection="3d")
-ax.scatter(points[:, 0], points[:, 1], points[:, 2])
-ax.set_axis_off()
-plt.show()
-
-"""
-To generate a `tf.data.Dataset()` we need to first parse through the ModelNet data
-folders. Each mesh is loaded and sampled into a point cloud before being added to a
-standard python list and converted to a `numpy` array. We also store the current
-enumerate index value as the object label and use a dictionary to recall this later.
-"""
+DATA_DIR = 'data/ModelNet10'
+NUM_POINTS = config['number_of_points']
+NUM_CLASSES = config['number_of_classes']
+BATCH_SIZE = config['batch_size']
 
 
-def parse_dataset(num_points=2048):
+def dataset_sample():
+    mesh = trimesh.load(os.path.join(DATA_DIR, "chair/train/chair_0001.off"))
+    mesh.show()
+    points = mesh.sample(NUM_POINTS)
+    fig = plt.figure(figsize=(5, 5))
+    ax = fig.add_subplot(111, projection="3d")
+    ax.scatter(points[:, 0], points[:, 1], points[:, 2])
+    ax.set_axis_off()
+    plt.show()
+
+
+def parse_dataset(num_points):
     train_points = []
     train_labels = []
     test_points = []
@@ -99,72 +61,45 @@ def parse_dataset(num_points=2048):
     )
 
 
-"""
-Set the number of points to sample and batch size and parse the dataset. This can take
-~5minutes to complete.
-"""
+if __name__ == "__main__":
 
-NUM_POINTS = config['number_of_points']
-NUM_CLASSES = config['number_of_classes']
-BATCH_SIZE = config['batch_size']
-epochs = config['epochs']
+    dataset_sample()
+    exit()
 
-train_points, test_points, train_labels, test_labels, CLASS_MAP = parse_dataset(
-    NUM_POINTS
-)
-
-"""
-Our data can now be read into a `tf.data.Dataset()` object. We set the shuffle buffer
-size to the entire size of the dataset as prior to this the data is ordered by class.
-Data augmentation is important when working with point cloud data. We create a
-augmentation function to jitter and shuffle the train dataset.
-"""
-
-
-def augment(points, label):
-    # jitter points
-    points += tf.random.uniform(points.shape, -0.005, 0.005, dtype=tf.float64)
-    # shuffle points
-    points = tf.random.shuffle(points)
-    return points, label
-
-
-train_dataset = tf.data.Dataset.from_tensor_slices((train_points, train_labels))
-test_dataset = tf.data.Dataset.from_tensor_slices((test_points, test_labels))
-
-train_dataset = train_dataset.shuffle(len(train_points)).map(augment).batch(BATCH_SIZE)
-test_dataset = test_dataset.shuffle(len(test_points)).batch(BATCH_SIZE)
-
-
-model = get_point_net_model()
-model.fit(train_dataset, epochs=epochs, validation_data=test_dataset)
-
-"""
-## Visualize predictions
-We can use matplotlib to visualize our trained model performance.
-"""
-
-data = test_dataset.take(1)
-
-points, labels = list(data)[0]
-points = points[:8, ...]
-labels = labels[:8, ...]
-
-# run test data through model
-preds = model.predict(points)
-preds = tf.math.argmax(preds, -1)
-
-points = points.numpy()
-
-# plot points with predicted class and label
-fig = plt.figure(figsize=(15, 10))
-for i in range(8):
-    ax = fig.add_subplot(2, 4, i + 1, projection="3d")
-    ax.scatter(points[i, :, 0], points[i, :, 1], points[i, :, 2])
-    ax.set_title(
-        "pred: {:}, label: {:}".format(
-            CLASS_MAP[preds[i].numpy()], CLASS_MAP[labels.numpy()[i]]
-        )
+    train_points, test_points, train_labels, test_labels, CLASS_MAP = parse_dataset(
+        NUM_POINTS
     )
-    ax.set_axis_off()
-plt.show()
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_points, train_labels))
+    test_dataset = tf.data.Dataset.from_tensor_slices((test_points, test_labels))
+
+    train_dataset = train_dataset.shuffle(len(train_points)).map(augment).batch(BATCH_SIZE)
+    test_dataset = test_dataset.shuffle(len(test_points)).batch(BATCH_SIZE)
+
+    model = get_point_net_model()
+    model.fit(train_dataset, epochs=20, validation_data=test_dataset)
+
+    data = test_dataset.take(1)
+
+    points, labels = list(data)[0]
+    points = points[:8, ...]
+    labels = labels[:8, ...]
+
+    # run test data through model
+    preds = model.predict(points)
+    preds = tf.math.argmax(preds, -1)
+
+    points = points.numpy()
+
+    # plot points with predicted class and label
+    fig = plt.figure(figsize=(15, 10))
+    for i in range(8):
+        ax = fig.add_subplot(2, 4, i + 1, projection="3d")
+        ax.scatter(points[i, :, 0], points[i, :, 1], points[i, :, 2])
+        ax.set_title(
+            "pred: {:}, label: {:}".format(
+                CLASS_MAP[preds[i].numpy()], CLASS_MAP[labels.numpy()[i]]
+            )
+        )
+        ax.set_axis_off()
+    plt.show()

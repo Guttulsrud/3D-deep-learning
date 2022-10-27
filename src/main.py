4f2d492cf5a770d554b2405e341aec39cab35f27
guttulsrud <guttulsrud@outlook.com>
2022-10-27 11:58:44 +0200

    Moved code to main and removed temp file

--- src/main.py
+++ src/main.py
@@ -1,38 +1,174 @@
-import medmnist
+import os
+import glob
+import trimesh
 import numpy as np
-from numpy import load
-import k3d
-from k3d.colormaps import paraview_color_maps
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from medmnist import INFO, Evaluator
-import plotly.graph_objects as go
+import tensorflow as tf
+from tensorflow.keras import layers
+from matplotlib import pyplot as plt
 
-data = load('../data/synapsemnist3d.npz')
-lst = data.files
+from src.network import get_model
 
-train_images, val_images, test_images = [], [], []
-train_labels, test_labels, val_labels = [], [], []
+"""
+Title: Point cloud classification with PointNet
+Author: [David Griffiths](https://dgriffiths3.github.io)
+Date created: 2020/05/25
+Last modified: 2020/05/26
+Description: Implementation of PointNet for ModelNet10 classification.
+"""
+"""
+# Point cloud classification
+"""
 
-for item in lst:
-    match item:
-        case 'train_images':
-            train_images = data[item]
-        case 'val_images':
-            val_images = data[item]
-        case 'test_images':
-            test_images = data[item]
-        case 'train_labels':
-            train_labels = data[item]
-        case 'val_labels':
-            val_labels = data[item]
-        case 'test_labels':
-            test_labels = data[item]
+"""
+## Introduction
+Classification, detection and segmentation of unordered 3D point sets i.e. point clouds
+is a core problem in computer vision. This example implements the seminal point cloud
+deep learning paper [PointNet (Qi et al., 2017)](https://arxiv.org/abs/1612.00593). For a
+detailed intoduction on PointNet see [this blog
+post](https://medium.com/@luis_gonzales/an-in-depth-look-at-pointnet-111d7efdaa1a).
+"""
 
-for image, label in zip(train_images, train_labels):
+"""
+## Setup
+If using colab first install trimesh with `!pip install trimesh`.
+"""
 
-    plt.imshow(image)
-    plt.show()
-    print('e+')
+tf.random.set_seed(1234)
 
-    exit()
+DATA_DIR = 'C:/Users/guttu/Desktop/Dev/PyCharm/3D-deep-learning/data/ModelNet10'
+
+"""
+We can use the `trimesh` package to read and visualize the `.off` mesh files.
+"""
+
+mesh = trimesh.load(os.path.join(DATA_DIR, "chair/train/chair_0001.off"))
+mesh.show()
+
+"""
+To convert a mesh file to a point cloud we first need to sample points on the mesh
+surface. `.sample()` performs a unifrom random sampling. Here we sample at 2048 locations
+and visualize in `matplotlib`.
+"""
+
+points = mesh.sample(2048)
+
+fig = plt.figure(figsize=(5, 5))
+ax = fig.add_subplot(111, projection="3d")
+ax.scatter(points[:, 0], points[:, 1], points[:, 2])
+ax.set_axis_off()
+plt.show()
+
+"""
+To generate a `tf.data.Dataset()` we need to first parse through the ModelNet data
+folders. Each mesh is loaded and sampled into a point cloud before being added to a
+standard python list and converted to a `numpy` array. We also store the current
+enumerate index value as the object label and use a dictionary to recall this later.
+"""
+
+
+def parse_dataset(num_points=2048):
+    train_points = []
+    train_labels = []
+    test_points = []
+    test_labels = []
+    class_map = {}
+    folders = glob.glob(os.path.join(DATA_DIR, "[!README]*"))
+
+    for i, folder in enumerate(folders):
+        print("processing class: {}".format(os.path.basename(folder)))
+        # store folder name with ID so we can retrieve later
+        class_map[i] = folder.split("/")[-1]
+        # gather all files
+        train_files = glob.glob(os.path.join(folder, "train/*"))
+        test_files = glob.glob(os.path.join(folder, "test/*"))
+
+        for f in train_files:
+            train_points.append(trimesh.load(f).sample(num_points))
+            train_labels.append(i)
+
+        for f in test_files:
+            test_points.append(trimesh.load(f).sample(num_points))
+            test_labels.append(i)
+
+    return (
+        np.array(train_points),
+        np.array(test_points),
+        np.array(train_labels),
+        np.array(test_labels),
+        class_map,
+    )
+
+
+"""
+Set the number of points to sample and batch size and parse the dataset. This can take
+~5minutes to complete.
+"""
+
+NUM_POINTS = 2048
+NUM_CLASSES = 10
+BATCH_SIZE = 32
+
+train_points, test_points, train_labels, test_labels, CLASS_MAP = parse_dataset(
+    NUM_POINTS
+)
+
+"""
+Our data can now be read into a `tf.data.Dataset()` object. We set the shuffle buffer
+size to the entire size of the dataset as prior to this the data is ordered by class.
+Data augmentation is important when working with point cloud data. We create a
+augmentation function to jitter and shuffle the train dataset.
+"""
+
+
+def augment(points, label):
+    # jitter points
+    points += tf.random.uniform(points.shape, -0.005, 0.005, dtype=tf.float64)
+    # shuffle points
+    points = tf.random.shuffle(points)
+    return points, label
+
+
+train_dataset = tf.data.Dataset.from_tensor_slices((train_points, train_labels))
+test_dataset = tf.data.Dataset.from_tensor_slices((test_points, test_labels))
+
+train_dataset = train_dataset.shuffle(len(train_points)).map(augment).batch(BATCH_SIZE)
+test_dataset = test_dataset.shuffle(len(test_points)).batch(BATCH_SIZE)
+
+"""
+### Build a model
+Each convolution and fully-connected layer (with exception for end layers) consits of
+Convolution / Dense -> Batch Normalization -> ReLU Activation.
+"""
+
+model = get_model(NUM_POINTS, NUM_CLASSES)
+model.fit(train_dataset, epochs=20, validation_data=test_dataset)
+
+"""
+## Visualize predictions
+We can use matplotlib to visualize our trained model performance.
+"""
+
+data = test_dataset.take(1)
+
+points, labels = list(data)[0]
+points = points[:8, ...]
+labels = labels[:8, ...]
+
+# run test data through model
+preds = model.predict(points)
+preds = tf.math.argmax(preds, -1)
+
+points = points.numpy()
+
+# plot points with predicted class and label
+fig = plt.figure(figsize=(15, 10))
+for i in range(8):
+    ax = fig.add_subplot(2, 4, i + 1, projection="3d")
+    ax.scatter(points[i, :, 0], points[i, :, 1], points[i, :, 2])
+    ax.set_title(
+        "pred: {:}, label: {:}".format(
+            CLASS_MAP[preds[i].numpy()], CLASS_MAP[labels.numpy()[i]]
+        )
+    )
+    ax.set_axis_off()
+plt.show()

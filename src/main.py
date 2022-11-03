dfb39dfb54d30c100dc5fe75395d5c3dc7cf3ff7
mathi <mathias_a_sandnes@hotmail.com>
2022-11-03 13:05:36 +0100

    added hpo

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import matplotlib
+import keras_tuner as kt
 
 from config import config
 from src.data_loader import get_dataset
@@ -12,7 +13,18 @@ matplotlib.use('TkAgg')
 if __name__ == "__main__":
     train_dataset, test_dataset, CLASS_MAP = get_dataset()
 
-    model = get_point_net_model()
-    model.fit(train_dataset, epochs=config['epochs'], validation_data=test_dataset)
+    tuner = kt.BayesianOptimization(get_point_net_model,
+                                    objective='val_accuracy')
 
-    show_performance(test_dataset, model, CLASS_MAP)
+    stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+
+    tuner.search(train_dataset, epochs=50, validation_split=0.2, callbacks=[stop_early])
+
+    # Get the optimal hyperparameters
+    best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+
+    print(f"""
+        The hyperparameter search is complete. The optimal number of units in the first densely-connected
+        layer is {best_hps.get('units')} and the optimal learning rate for the optimizer
+        is {best_hps.get('learning_rate')}.
+    """)
--- src/network.py
+++ src/network.py
@@ -15,32 +15,29 @@ close to an orthogonal matrix (i.e. ||X*X^T - I|| = 0).
 """
 
 
-def get_point_net_model():
+def get_point_net_model(hp):
     """
     The main network can be then implemented in the same manner where the t-net mini models
     can be dropped in a layers in the graph. Here we replicate the network architecture
     published in the original paper but with half the number of weights at each layer as we
     are using the smaller 10 class ModelNet dataset.
     """
-
     number_of_points = config['number_of_points']
     number_of_classes = config['number_of_classes']
-    learning_rate = config['learning_rate']
 
     inputs = keras.Input(shape=(number_of_points, 3))
-
     x = get_t_net(inputs, 3)
-    x = conv_bn(x, 32)
-    x = conv_bn(x, 32)
-    x = get_t_net(x, 32)
-    x = conv_bn(x, 32)
-    x = conv_bn(x, 64)
-    x = conv_bn(x, 512)
+    x = conv_bn(x, 32, hp)
+    x = conv_bn(x, 32, hp)
+    x = get_t_net(x, 32, hp)
+    x = conv_bn(x, 32, hp)
+    x = conv_bn(x, 64, hp)
+    x = conv_bn(x, 512, hp)
     x = layers.GlobalMaxPooling1D()(x)
-    x = dense_bn(x, 256)
-    x = layers.Dropout(0.3)(x)
-    x = dense_bn(x, 128)
-    x = layers.Dropout(0.3)(x)
+    x = dense_bn(x, 256, hp)
+    x = layers.Dropout(hp.Choice('dropout', values=config['hpo']['dropout1']))(x)
+    x = dense_bn(x, 128, hp)
+    x = layers.Dropout(hp.Choice('dropout', values=config['hpo']['dropout2']))(x)
 
     outputs = layers.Dense(number_of_classes, activation="softmax")(x)
 
@@ -49,42 +46,59 @@ def get_point_net_model():
 
     model.compile(
         loss="sparse_categorical_crossentropy",
-        optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
+        optimizer=keras.optimizers.Adam(learning_rate=config['network']['learning_rate']),
         metrics=["sparse_categorical_accuracy"],
     )
 
     return model
 
 
-def get_t_net(inputs, num_features):
+def get_t_net(inputs, num_features, hp=None):
+    num_features = num_features * 2 if hp else num_features
     # Initialise bias as the identity matrix
     bias = keras.initializers.Constant(np.eye(num_features).flatten())
     reg = OrthogonalRegularizer(num_features)
 
-    x = conv_bn(inputs, 32)
-    x = conv_bn(x, 64)
-    x = conv_bn(x, 512)
+    x = conv_bn(inputs, 32, hp)
+    x = conv_bn(x, 64, hp)
+    x = conv_bn(x, 512, hp)
     x = layers.GlobalMaxPooling1D()(x)
-    x = dense_bn(x, 256)
-    x = dense_bn(x, 128)
-    x = layers.Dense(
-        num_features * num_features,
-        kernel_initializer="zeros",
-        bias_initializer=bias,
-        activity_regularizer=reg,
-    )(x)
+    x = dense_bn(x, 256, hp)
+    x = dense_bn(x, 128, hp)
+    x = layers.Dense(num_features * num_features,
+                     kernel_initializer="zeros",
+                     bias_initializer=bias,
+                     activity_regularizer=reg,
+                     )(x)
     feat_T = layers.Reshape((num_features, num_features))(x)
+
+    print(feat_T.shape, inputs.shape)
     # Apply affine transformation to input features
     return layers.Dot(axes=(2, 1))([inputs, feat_T])
 
 
-def conv_bn(x, filters):
+def conv_bn(x, filters, hp=None):
+    if hp:
+        filters = hp.Int('units', min_value=filters * 2, max_value=filters * 4, step=32)
+
     x = layers.Conv1D(filters, kernel_size=1, padding="valid")(x)
     x = layers.BatchNormalization(momentum=0.0)(x)
-    return layers.Activation("relu")(x)
+
+    activation = hp.Choice('activation', values=config['hpo']['activation']) if hp else 'relu'
+    return layers.Activation(activation)(x)
 
 
-def dense_bn(x, filters):
+def dense_bn(x, filters, hp=None):
+    if hp:
+        filters = hp.Int('units', min_value=filters * 2, max_value=filters * 4, step=32)
+
     x = layers.Dense(filters)(x)
     x = layers.BatchNormalization(momentum=0.0)(x)
-    return layers.Activation("relu")(x)
+
+    activation = hp.Choice('activation', values=config['hpo']['activation']) if hp else 'relu'
+    return layers.Activation(activation)(x)
+
+# Tensorboard
+# HPO Stuff
+# metrics?
+# sparse_categorical_accuracy

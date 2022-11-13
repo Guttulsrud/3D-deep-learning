97e76ddb2b0f754c9b0aa953ad384d09d3f54fea
guttulsrud <guttulsrud@outlook.com>
2022-11-13 16:17:08 +0100

    Added conditional to save preprocessed dataset in the dataloader

--- src/main.py
+++ src/main.py
@@ -5,14 +5,19 @@ from config import config
 from src.data_loader import get_dataset
 from src.evaluation import show_performance
 from src.network import get_point_net_model
+from src.shapenet_dataloader import ShapeNetDataLoader
 
 tf.random.set_seed(config['random_seed'])
 matplotlib.use('TkAgg')
 
 if __name__ == "__main__":
-    train_dataset, test_dataset, CLASS_MAP = get_dataset()
-
+    train_dataset, test_dataset, CLASS_MAP = get_dataset(load_file=True)
+    # print(train_dataset)
+    # dl = ShapeNetDataLoader()
+    # train_dataset = dl.get_train_data()
+    # test_dataset = dl.get_test_data()
+    # print(train_dataset)
     model = get_point_net_model()
-    model.fit(train_dataset, epochs=config['epochs'], validation_data=test_dataset)
+    model.fit(test_dataset, epochs=config['epochs'], validation_data=test_dataset)
 
     show_performance(test_dataset, model, CLASS_MAP)

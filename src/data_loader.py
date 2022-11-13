68904f5c2ffeb9e1fa3012fcee40050ecf8781ee
guttulsrud <guttulsrud@outlook.com>
2022-11-13 17:17:14 +0100

    Modified PointNet architecture to original paper architecture. Added logging of results

--- src/data_loader.py
+++ src/data_loader.py
@@ -21,9 +21,9 @@ def dataset_sample():
     plt.show()
 
 
-def parse_dataset(num_points, load_file=False):
+def parse_dataset(num_points, load_file=None):
     if load_file:
-        f = open('data.json')
+        f = open(load_file)
         result = json.load(f)
         class_map = {int(k): v for k, v in result['class_map'].items()}
 
@@ -65,7 +65,9 @@ def parse_dataset(num_points, load_file=False):
         'test_labels': np.array(test_labels).tolist(),
         'class_map': class_map
     }
-    with open("data.json", "w") as outfile:
+    dataset = config['data_dir'].split('data/')[1]
+
+    with open(f'{dataset}.json', "w") as outfile:
         outfile.write(json.dumps(result))
 
     return (
@@ -77,7 +79,7 @@ def parse_dataset(num_points, load_file=False):
     )
 
 
-def get_dataset(load_file):
+def get_dataset(load_file=None):
     train_points, test_points, train_labels, test_labels, CLASS_MAP = parse_dataset(
         config['number_of_points'], load_file=load_file
     )
--- src/main.py
+++ src/main.py
@@ -6,19 +6,14 @@ from src.data_loader import get_dataset
 from src.evaluation import show_performance
 from src.network import get_point_net_model
 from src.shapenet_dataloader import ShapeNetDataLoader
+from src.utils import save_results
 
 tf.random.set_seed(config['random_seed'])
 matplotlib.use('TkAgg')
 
 if __name__ == "__main__":
-    train_dataset, test_dataset, CLASS_MAP = get_dataset(load_file=True)
-
-    # print(train_dataset)
-    # dl = ShapeNetDataLoader()
-    # train_dataset = dl.get_train_data()
-    # test_dataset = dl.get_test_data()
-    # print(train_dataset)
+    train_dataset, test_dataset, CLASS_MAP = get_dataset(load_file='ModelNet10.json')
     model = get_point_net_model()
-    model.fit(test_dataset, epochs=config['epochs'], validation_data=test_dataset)
-
+    results = model.fit(train_dataset, epochs=config['epochs'], validation_data=test_dataset)
+    save_results(results.history)
     show_performance(test_dataset, model, CLASS_MAP)
